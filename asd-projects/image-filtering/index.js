ed8ab7530b05fb5e48deef2300d064e5a050257d
SashaMC <sasha.canales@inspire35.org>
2026-04-23 20:23:15 +0000

    message

--- asd-projects/image-filtering/index.js
+++ asd-projects/image-filtering/index.js
@@ -20,7 +20,7 @@ function resetAndRender() {
 // all of your apply functions
 function applyAndRender() {
   // Multiple TODOs: Call your apply function(s) here
-
+applyFilter();
   
 
   // do not change the below line of code
@@ -32,7 +32,9 @@ function applyAndRender() {
 /////////////////////////////////////////////////////////
 
 // TODO 1, 2, 3 & 5: Create the applyFilter function here
-
+function applyFilter (){
+  console.log("Filter applied");
+}
 
 // TODO 9 Create the applyFilterNoBackground function
 
